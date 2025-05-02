b8a4cd19e3a6583758a4a40bdf21f769357d7f44
Konfus-dev <63468146+Konfus-dev@users.noreply.github.com>
2025-05-01 18:28:52 -0700

    Remove unused stuff and adding premake file

---
+++ googletest/premake5.lua
@@ -0,0 +1,43 @@
+project "googletest"
+    kind "StaticLib"
+    language "C++"
+    cppdialect "C++20"
+    staticruntime "Off"
+
+    if OutputIntermediateDir == nil or OutputTargetDir == nil then
+        targetdir ("Build/bin/%{prj.name}/")
+        objdir    ("Build/obj/%{prj.name}/")
+
+    else
+        targetdir ("../../../" .. OutputTargetDir .. "")
+        objdir    ("../../../" .. OutputIntermediateDir .. "")
+    end
+
+    files 
+    {
+        "./src/gtest-all.cc",
+        "./src/gtest_main.cc",
+        "./include/**.h"
+    }
+
+    includedirs 
+    {
+        "./",
+        "./include"
+    }
+
+    filter "configurations:Debug"
+        runtime "Debug"
+        linkoptions { "/MDd" }  -- Enable Link Time Code Generation and Incremental linking
+        symbols "on"
+
+    filter "configurations:Optimized"
+        runtime "Release"
+        linkoptions { "/MDd" }  -- Enable Link Time Code Generation and Incremental linking
+        optimize "on"
+
+    filter "configurations:Release"
+        runtime "Release"
+        optimize "on"
+        linkoptions { "/MD" }  -- Enable Link Time Code Generation and Incremental linking
+        symbols "off"
